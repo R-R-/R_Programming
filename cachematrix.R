7a21f63669086288990ed1b6ec12e407e4c278eb
Trung <ngthanhtrung23@gmail.com>
2014-04-09 01:13:55 +0800

    Move comment around

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,3 @@
-# makeCacheMatrix: return a list of functions to:
-# 1. Set the value of the matrix
-# 2. Get the value of the matrix
-# 3. Set the value of the inverse
-# 4. Get the value of the inverse
-
-# cacheSolve: Compute the inverse of the matrix. If the inverse is already
-# calculated before, it returns the cached inverse.
-
 # Example usage:
 # > x <- matrix(rnorm(16), nrow = 4)          // Create a matrix x
 # > cx <- makeCacheMatrix(x)                  // Create our special matrix
@@ -15,6 +6,12 @@
 # > cacheSolve(cx)                            // Call the 2nd time, so return
 #                                             // the cached inverse
 
+
+# makeCacheMatrix: return a list of functions to:
+# 1. Set the value of the matrix
+# 2. Get the value of the matrix
+# 3. Set the value of the inverse
+# 4. Get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
     # inv will store the cached inverse matrix
     inv <- NULL
@@ -37,8 +34,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve: Compute the inverse of the matrix. If the inverse is already
+# calculated before, it returns the cached inverse.
 cacheSolve <- function(x, ...) {
     inv <- x$getinv()
 
